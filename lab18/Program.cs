633a35e16497d36933a4cd26c85ad62b3abf84d4
mrmarkiz <maxtomusiak315@gmail.com>
2023-05-30 18:56:11 +0300

    Add project files.

---
+++ lab18/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace lab18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.Write("Enter task to run: ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+
+                }
+            } while (choice != 0);
+        }
+    }
+}
